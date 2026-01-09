65f302002b39012a17812d1c1708a4b4b93e8e88
shashanksavarkar17 <savarkar.shashank_btech24@gsv.ac.in>
2026-01-09 20:02:32 +0530

    Added Registration Form

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -4,36 +4,57 @@ import Footer from "../components/Footer";
 import "./Registration.css";
 
 const Registration = () => {
+  const openZohoForm = () => {
+    const url =
+      "https://forms.zohopublic.in/operagxshashank17gm1/form/InternshipStudentInformationForm/formperma/nhMsMjM-26-9jmeUuaQVGJ4e13Ahz8OivB-uFgwY30Y";
+
+    const width = 700;
+    const height = 650;
+
+    const left = window.screen.width / 2 - width / 2;
+    const top = window.screen.height / 2 - height / 2;
+
+    window.open(
+      url,
+      "ZohoForm",
+      `width=${width},
+       height=${height},
+       left=${left},
+       top=${top},
+       toolbar=0,
+       location=0,
+       status=1,
+       scrollbars=1,
+       resizable=1`
+    );
+  };
+
   return (
-      <>
-        <Navbar />
+    <>
+      <Navbar />
 
-        <div className="register-wrapper">
-          <div className="register-card google-form-card">
-            <span className="step-indicator">Team Registration</span>
+      <div className="register-wrapper">
+        <div className="register-card google-form-card">
+          <span className="step-indicator">Team Registration</span>
 
-            <h1 className="google-form-title">
-              Official Team Registration Form
-            </h1>
+          <h1 className="google-form-title">Official Team Registration Form</h1>
 
-            <p className="google-form-desc">
-              Please fill the form carefully. This is the <strong>only official way</strong>
-              to register your team.
-            </p>
+          <p className="google-form-desc">
+            Please fill the form carefully. This is the{" "}
+            <strong>only official way</strong> to register your team.
+          </p>
 
-            {/* GOOGLE FORM EMBED */}
-            <div className="google-form-container">
-              <iframe
-                  src="https://docs.google.com/forms/d/e/1FAIpQLScxyqhGSL8YL73D1EVWPV0nyCXy5NUDOg-hN5-L2EKegQ_KJQ/viewform?embedded=true"
-                  width="700" height="700" frameBorder="0" marginHeight="0" marginWidth="0">Loading…
-              </iframe>
-
-            </div>
+          {/* ZOHO FORM TRIGGER */}
+          <div className="google-form-container">
+            <button className="zoho-open-btn" onClick={openZohoForm}>
+              Access Registration Form
+            </button>
           </div>
         </div>
+      </div>
 
-        <Footer/>
-      </>
+      <Footer />
+    </>
   );
 };
 
